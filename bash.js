75e547d5ad2c6f0691e96c06627627fb9232493d
chrisstpehenmiller <chrismiller@cm.local>
2018-03-05 16:24:19 -0600

    refactoring

--- bash.js
+++ bash.js
@@ -1,18 +1,17 @@
 let commands = require('./commands');
 
-
 // Output a prompt
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-    var cmd = data.toString().trim(); // remove the newline
-    var args = cmd.split(' ');
-    var fileName = args.slice(1);
-
-
-    commands[args[0]](fileName)
-
-    process.stdout.write('\nprompt > ');
+    var tokens = data.toString().trim().split(' ');
+    var cmd = tokens[0];
+    var args = tokens.slice(1);
+    commands[cmd](args, done)
 });
+
+function done (output) {
+    process.stdout.write(output + '\nprompt > ');
+}
 
--- commands.js
+++ commands.js
@@ -1,87 +1,90 @@
 let request = require('request');
 let fs = require('fs');
 
-module.exports = commands;
-
-function done () {
-    process.stdout.write("prompt > ");
-}
-
 let commands = {
-    pwd: function (file) {
-        process.stdout.write(process.cwd());
+
+    pwd: function (args, done) {
+        let output = process.cwd();
+        done(output);
     },
-    ls: function (file) {
+    
+    ls: function (args, done) {
         fs.readdir('.', function (err, files) {
             if (err) throw err;
-            files.forEach(function (file) {
-                process.stdout.write(file.toString() + "\n");
-            })
+            let output = files.join('\n');
+            done(output)
         });
     },
-    echo: function (file) {
-        process.stdout.write(file.join(' '));
+    echo: function (args, done) {
+        if (args[0] = '$') {
+            commands.args[slice(1)]()
+        }
+        let output = args.join(' ')
+        done(output);
     },
-    cat: function (file) {
-        let fileName = './' + file.toString();
-        fs.readFile(fileName, function (err, file) {
+    cat: function (args, done) {
+        let file = './' + args;
+        fs.readFile(file, function (err, data) {
             if (err) throw err;
-            process.stdout.write(file);
+            let output = data;
+            done(output);
         })
     },
-    head: function (file) {
-        let fileName = './' + file.toString();
-        fs.readFile(fileName, function (err, file) {
+    head: function (args, done) {
+        let file = './' + args;
+        fs.readFile(file, function (err, data) {
             if (err) throw err;
-            let body = file.toString().split('\n').slice(0, 5).join('\n');
-            process.stdout.write(body)
+            let output = data.toString().split('\n').slice(0, 5).join('\n');
+            done(output);
         });
     },
-    tail: function (file) {
-        let fileName = './' + file.toString();
-        fs.readFile(fileName, function (err, file) {
+    tail: function (args, done) {
+        let file = './' + args.toString();
+        fs.readFile(file, function (err, data) {
             if (err) throw err;
-            let body = file.toString().split('\n').slice(-6).join('\n');
-            process.stdout.write(body)
+            let output = data.toString().split('\n').slice(-6).join('\n');
+            done(output);
         });
     },
-    sort: function (file) {
-        let fileName = './' + file.toString();
-        fs.readFile(fileName, function (err, file) {
+    sort: function (args, done) {
+        let file = './' + args.toString();
+        fs.readFile(file, function (err, data) {
             if (err) throw err;
-            let body = file.toString().split('\n').sort().join('\n');
-            process.stdout.write(body)
+            let output = data.toString().split('\n').sort().join('\n');
+            done(output);
         });
     },
-    wc: function (file) {
-        let fileName = './' + file.toString();
-        fs.readFile(fileName, function (err, file) {
+    wc: function (args, done) {
+        let file = './' + args.toString();
+        fs.readFile(fileName, function (err, data) {
             if (err) throw err;
-            let count = file.toString().split('\n').length.toString();
-            process.stdout.write(count)
+            let output = data.toString().split('\n').length.toString();
+            done(output);
         });
     },
-    uniq: function (file) {
-        let fileName = './' + file.toString();
-        fs.readFile(fileName, function (err, file) {
+    uniq: function (args, done) {
+        let file = './' + args.toString();
+        fs.readFile(file, function (err, data) {
             if (err) throw err;
-            let body = file.toString().split('\n').sort();
-            let newBody = [];
-            for (let i = 0; i < body.length; i++) {
-                if (body[i] !== newBody[newBody.length - 1]) {
-                    newBody.push(body[i])
+            let lines = data.toString().split('\n').sort();
+            let uniqueLines = [];
+            for (let i = 0; i < lines.length; i++) {
+                if (lines[i] !== uniqueLines[uniqueLines.length - 1]) {
+                    uniqueLines.push(lines[i])
                 };
             }
-            process.stdout.write(newBody.join('\n'))
+            let output = uniqueLines.join('\n');
+            done(output);
         });
     },
-    curl: function (url) {
+    curl: function (url, done) {
         let fullUrl = 'http://' + url;
         request(fullUrl, function (error, response, body) {
-            process.stdout.write(body)
+            let output = body;
+            done(output)
         });
     }
+
 }
 
-
-
+module.exports = commands;
